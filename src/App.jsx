b03265fec1eeb9957bf565f78583603af0206d7f
Arjun M Menon <arjun@techgentsia.com>
2026-03-06 15:56:10 +0530

    Day 8: Dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,30 @@
-import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import UserDetails from "./pages/UserDetails";
+
 
 function App() {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = () => {
-    if (!task.trim()) return;
-
-    const newTodo = {
-      id: Date.now(),
-      text: task,
-      completed: false,
-    };
-
-    setTodos([...todos, newTodo]);
-    setTask("");
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      )
-    );
-  };
-
   return (
-    <div style={{ padding: "20px", maxWidth: "400px" }}>
-      <h1>Todo App</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>My React App</h1>
 
-      <input
-        type="text"
-        placeholder="Enter task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/users">Users</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
 
-      <button onClick={addTodo}>Add</button>
-
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} style={{ marginTop: "10px" }}>
-            <span
-              onClick={() => toggleTodo(todo.id)}
-              style={{
-                cursor: "pointer",
-                textDecoration: todo.completed ? "line-through" : "none",
-              }}
-            >
-              {todo.text}
-            </span>
-
-            <button
-              onClick={() => deleteTodo(todo.id)}
-              style={{ marginLeft: "10px" }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-      <p>Total Tasks: {todos.length}</p>
-      <p>Completed Tasks: {todos.filter((todo) => todo.completed).length}</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Users() {
-  return <h2>Users Page</h2>;
+  const users = [
+    { id: 1, name: "Arjun" },
+    { id: 2, name: "Rahul" },
+    { id: 3, name: "Neha" }
+  ];
+
+  return (
+    <div>
+      <h2>Users Page</h2>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <Link to={`/users/${user.id}`}>
+            {user.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Users;
